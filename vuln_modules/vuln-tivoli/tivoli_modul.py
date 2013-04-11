bbb87941e29fd80ee649f795f8ae93a5a33605ae
zeroq <jan-go@gmx.de>
2013-04-11 19:39:06 +0200

    updated copyright date

--- vuln_modules/vuln-tivoli/tivoli_modul.py
+++ vuln_modules/vuln-tivoli/tivoli_modul.py
@@ -1,6 +1,6 @@
 """
 [Amun - low interaction honeypot]
-Copyright (C) [2008]  [Jan Goebel]
+Copyright (C) [2013]  [Jan Goebel]
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
