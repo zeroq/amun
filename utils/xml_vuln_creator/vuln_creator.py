d6e082e2356fcfb5d4db6fa94a4648264e88dad3
zeroq <jan-go@gmx.de>
2014-03-21 22:05:49 +0100

    updated copyright

--- utils/xml_vuln_creator/vuln_creator.py
+++ utils/xml_vuln_creator/vuln_creator.py
@@ -2,7 +2,7 @@
 
 """
 [Amun - low interaction honeypot]
-Copyright (C) [2013]  [Jan Goebel]
+Copyright (C) [2014]  [Jan Goebel]
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
